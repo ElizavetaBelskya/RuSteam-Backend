33bcc29a1bf441f815a026f83e3df306eec44046
Fattakhov Oscar <ockap20030408@gmail.com>
2023-03-03 00:45:03 +0300

    Spring Boot project with fold structure created

---
+++ src/main/java/ru/itis/rusteam/RuSteamApplication.java
@@ -0,0 +1,13 @@
+package ru.itis.rusteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuSteamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuSteamApplication.class, args);
+    }
+
+}
