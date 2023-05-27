c004d84da4ea1fbd0c585afac89c5eb468b13b65
Timerbulat <timerhelmut@gmail.com>
2023-05-27 15:22:35 +0300

    Added checking unique email and nickname

---
+++ src/main/java/ru/itis/rusteam/security/exceptions/AlreadyExistsException.java
@@ -0,0 +1,10 @@
+package ru.itis.rusteam.security.exceptions;
+
+public class AlreadyExistsException extends RuntimeException{
+    public AlreadyExistsException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public AlreadyExistsException(String message){
+        super(message);
+    }
+}
